7715c023851ca2a906b22bd2801b9a7441212c72
Sujeet Gholap <sujeetgholap@gmail.com>
2013-06-10 04:40:33 +0530

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ fetch-url
 =========
 
 A nifty URL fetcher returning source in JSONP.
+
+Example
+-------
+```
+function myCallback (json) {
+    var page_source = json.source;
+    // do stuff with page source here.
+}
+
+var url = "http://google.com";
+
+$.ajax({
+  url: "http://fetch-url.appspot.com?"
+               + "?url=" + url
+               + "&funcname=myCallback",
+  dataType: "jsonp"
+});
+```
--- fetch-url.py
+++ fetch-url.py
@@ -7,7 +7,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
         source_code = urlfetch.fetch (self.request.GET['url']).content
-        function_name = self.request.GET ['callback']
+        function_name = self.request.GET ['funcname']
         self.response.write("%s(%s);"%(function_name,json.dumps({'source':source_code})))
 
 application = webapp2.WSGIApplication([
