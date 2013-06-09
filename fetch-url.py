75ebc7f08811072aefa58bc1766f7d01aba84350
Sujeet Gholap <sujeetgholap@gmail.com>
2013-06-10 04:18:27 +0530

    Initial commit.

---
+++ fetch-url.py
@@ -0,0 +1,15 @@
+import webapp2, json
+
+from google.appengine.api import urlfetch
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        source_code = urlfetch.fetch (self.request.GET['url']).content
+        function_name = self.request.GET ['callback']
+        self.response.write("%s(%s);"%(function_name,json.dumps({'source':source_code})))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
